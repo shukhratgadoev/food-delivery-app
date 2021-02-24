5b63ad15657f5015582d227133f5c90de83a449b
unknown <shukhratgadoev@gmail.com>
2021-02-25 02:56:34 +0500

    feat: Add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './style.css';
+import logo from '../../images/logo.svg';
+
+export const Header = () => {
+	return (
+		<div className='wrap'>
+			<div>
+				<img src={logo} alt='logo' />
+			</div>
+			<ul>
+				<li>
+					<a href='//#region'>Home</a>
+				</li>
+				<li>
+					<a href='//#region'>Product</a>
+				</li>
+				<li>
+					<a href='//#region'>Faq</a>
+				</li>
+				<li>
+					<a href='//#region'>Contact</a>
+				</li>
+			</ul>
+		</div>
+	);
+};
